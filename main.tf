851bc6e41121dbeade4ce3a71e28130e4a85ff34
Gregg Ellis Schofield <schofieldgregg@gmail.com>
2022-05-01 11:41:33 +0100

    Configure remote state in main.tf Add the -upgrade flag to the terraform init command in the GitHub workflow

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 terraform {
+
+  # The configuration for the `remote` backend.
+  backend "remote" {
+    # The name of your Terraform Cloud organisation.
+    organization = "example-organisation"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "example-workspace"
+    }
+  }
+
   required_providers {
     pagerduty = {
-      source = "PagerDuty/pagerduty"
+      source  = "PagerDuty/pagerduty"
       version = "2.4.1"
     }
   }
